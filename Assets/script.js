a8dcb60020f3365047e51e21db8e5145406ce4d9
jugrajn <jugraj.nijjar@gmail.com>
2021-05-24 17:17:34 -0400

    array of objects  created (questions and choices)

--- Assets/script.js
+++ Assets/script.js
@@ -2,11 +2,42 @@
 var timeElement = document.querySelector(".time");
 var secondsLeft = 5;
 
+// Question and Answers variables
+var questionEl = document.querySelector("#questionEl");
+var choicesEl = document.querySelector("#choicesEl");
+
 //Button variable
 var startButton = document.querySelector(".start-button");
 
 //Questions of Object with Answer Value Arrays
 
+let quizQuestions = [
+    {
+        question: "Commonly used data types DO NOT include?",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
+    },
+    {
+        question: "The condition in a if/else statement is enclosed within an __",
+        choices: ["Quotes", "Curly Brackets", "Parantheses", "Square Brackets"],
+        answer: "Parantheses"
+    },
+    {
+        question: "What do we call properties inside an object?",
+        choices: ["Methods", "Functions", "Element Names", "Key Value Pairs"],
+        answer: "Kay Value Pairs"
+    },
+    {
+        question: "Commonly used data types DO NOT include?",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
+    },
+    {
+        question: "Commonly used data types DO NOT include?",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
+    },
+]
 
 
 
