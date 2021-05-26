db185d3b8f7066082ad4b189398695a297565cb8
jugrajn <jugraj.nijjar@gmail.com>
2021-05-25 22:21:36 -0400

    fixed button targeting and adjusted variable naming conventions

--- Assets/script.js
+++ Assets/script.js
@@ -6,12 +6,16 @@ var secondsLeft = 75;
 var questionEl = document.querySelector("#questionEl");
 var choicesEl = document.querySelector("#choicesEl");
 
+// Score variable
+var scoreEl = document.querySelector(".score");
+var score = 0;
+
 //Button variable
 var startButton = document.querySelector(".start-button");
 
 //Questions of Object with Answer Value Arrays
 
-let quizQuestions = [
+let quiz = [
     {
         question: "Commonly used data types DO NOT include?",
         choices: ["Strings", "Booleans", "Alerts", "Numbers"],
@@ -25,7 +29,7 @@ let quizQuestions = [
     {
         question: "What do we call properties inside an object?",
         choices: ["Methods", "Functions", "Element Names", "Key Value Pairs"],
-        answer: "Kay Value Pairs"
+        answer: "Key Value Pairs"
     },
     {
         question: "Arrays in JavaScript can be used to store?",
@@ -36,7 +40,7 @@ let quizQuestions = [
         question: "What is the file extension for a Javascript file?",
         choices: [".java", ".js", ".html", ".css"],
         answer: ".js"
-    },
+    }
 ]
 
 // Need variable to track which Question im on in the object
@@ -51,55 +55,59 @@ var timerInterval = setInterval(function() {
     secondsLeft--;
     timeElement.textContent = secondsLeft + " seconds left until Game Over";
 
-    if(secondsLeft === 0) {
+    if(secondsLeft <= 0 || whichQuestion === quiz.length) {
         clearInterval(timerInterval);
-        sendMessage();
-        return;
+        storeScore();
         }
 
     }, 1000);
 }
 
-function sendMessage() {
-    timeElement.textContent = "Game Over";
-}
+// function sendMessage() {
+//     timeElement.textContent = "Game Over";
+// }
 
 //----------------------------------------------------------------
 // Show Questions Function
 
 function showQuiz() {
-    if (whichQuestion < quizQuestions.length) {
-        questionEl.textContent = quizQuestions[whichQuestion].question;
+        questionEl.textContent = quiz[whichQuestion].question;
         choicesEl.textContent = "";
 
-        for ( let i = 0; i < quizQuestions[whichQuestion].choices.length; i++) {
+        for ( let i = 0; i < quiz[whichQuestion].choices.length; i++) {
             //Create button elements to each choice for each question and append on html
             var newButton = document.createElement("button"); 
-            newButton.textContent = quizQuestions[whichQuestion].choices[i];
-            newButton.addEventListener("click", function (event) {
-                event.stopPropagation();
-
-                if (newButton.textContent === quizQuestions[whichQuestion].answer) {
-                    alert("Correct"); // NEED TO STORE POINTS
-                    newButton.style.color = "green";
-                }   else {
-                    secondsLeft = secondsLeft - 15;
-                }
-
-                questionEl.textContent = "";
-
-                if (whichQuestion === quizQuestions.length) {
-                    return;
-                } else {
-                    whichQuestion++;
-                    showQuiz();
-                }
-            });
+            newButton.textContent = quiz[whichQuestion].choices[i];
+            newButton.setAttribute("data-id", i);
+            newButton.addEventListener("click", evaluateAnswer);
 
             choicesEl.append(newButton);
         }
-    }
+    
 }
+
+function evaluateAnswer(event){
+    event.stopPropagation();
+    console.log(event.target.textContent)
+    console.log(quiz[whichQuestion].answer)
+    if (event.target.textContent !== quiz[whichQuestion].answer) {
+        secondsLeft = secondsLeft - 15;
+        score = score - 10;
+    }   else {
+        score = score + 100;
+    }
+    questionEl.textContent = "";
+
+    if (whichQuestion === quiz.length) {
+        //
+        return;
+    } else {
+        whichQuestion++;
+        showQuiz();
+    }
+
+}
+
 
 
 //Button Event Listener
